4e10a7818ab39d9cfc2f54080bb04c00f4c2db97
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-13 19:49:57 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+var request = require('request'),
+    util = require('./util'),
+    pickInputs = {
+        workflow_id: { key: 'workflow_id', validate: { req: true } }
+    },
+    pickOutputs = {
+        total_items: 'total_items',
+        _links: '_links',
+        id: { key: 'automations', fields: ['id'] },
+        status: { key: 'automations', fields: ['status'] },
+        emails_sent: { key: 'automations', fields: ['emails_sent'] },
+        recipients: { key: 'automations', fields: ['recipients'] },
+        tracking: { key: 'automations', fields: ['tracking'] },
+        report_summary: { key: 'automations', fields: ['report_summary'] }
+    };
+
 module.exports = {
+    authOptions: function (dexter) {
+        if (!dexter.environment('mailchimp_api_key') || !dexter.environment('server')) {
+            this.fail('A [mailchimp_api_key] and [server] environment variables are required for this module');
+            return false;
+        } else {
+            return {
+                api_key: dexter.environment('mailchimp_api_key'),
+                server: dexter.environment('server')
+            }
+        }
+    },
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +33,31 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var auth = this.authOptions(dexter),
+            inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
+
+        if (!auth) return;
+
+        if (validateErrors)
+            return this.fail(validateErrors);
+
+        request({
+            method: 'POST',
+            baseUrl: 'https://' + auth.server + '.api.mailchimp.com/3.0',
+            uri: 'automations/' + inputs.workflow_id + '/actions/start-all-emails',
+            json: true,
+            auth: {
+                username: 'api_key',
+                password: auth.api_key
+            }
+        },
+        function (error, response, body) {
+            if (!error && (response.statusCode === 200 || response.statusCode === 204)) {
+                this.complete({});
+            } else {
+                this.fail(error || body);
+            }
+        }.bind(this));
     }
 };
